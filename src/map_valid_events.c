37fc71e2a4a95461f06e18411fb6f10ac7f5ce11
Daniel-0liver <dtoliverbr@gmail.com>
2022-04-27 01:59:40 +0100

    movent updtades error

--- src/map_valid_events.c
+++ src/map_valid_events.c
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   map_valid_events.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/10 01:36:13 by dateixei          #+#    #+#             */
-/*   Updated: 2022/04/27 01:40:02 by dateixei         ###   ########.fr       */
+/*   Created: 2022/04/27 01:14:36 by dateixei          #+#    #+#             */
+/*   Updated: 2022/04/27 01:14:49 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
-{
-	t_game	game;
-	
-	game_memory_allocation(&game);
-	game.mlx = mlx_init();
-	game_init(&game);
-	get_hooks(&game);
-	mlx_loop(game.mlx);
-}
--- src/player_events.c
+++ src/player_events.c
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 21:33:50 by dateixei          #+#    #+#             */
-/*   Updated: 2022/04/27 00:20:31 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/04/27 00:25:22 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	player_move_right(t_game *game)
 {
-	game->player->player_pos_x += 10;
+	game->player->player_pos_x += 50;
 	return ; 
 }
 
 void	player_move_left(t_game *game)
 {
-	game->player->player_pos_x -= 10;
+	game->player->player_pos_x -= 50;
 	return ; 
 }
 
 void	player_move_up(t_game *game)
 {
-	game->player->player_pos_y -= 10;
+	game->player->player_pos_y -= 50;
 	return ; 
 }
 
 void	player_move_down(t_game *game)
 {
-	game->player->player_pos_y += 10;
+	game->player->player_pos_y += 50;
 	return ; 
 }
