4639dde5c0896a459d96661ef1605676d0d68230
Daniel-0liver <dtoliverbr@gmail.com>
2022-06-26 23:20:31 +0100

    Correct map path

--- src/input_valid_events.c
+++ src/input_valid_events.c
@@ -6,7 +6,7 @@
 /*   By: dateixei <dateixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 21:00:52 by dateixei          #+#    #+#             */
-/*   Updated: 2022/06/22 23:52:22 by dateixei         ###   ########.fr       */
+/*   Updated: 2022/06/25 15:57:37 by dateixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	input_valid(int argc, char *map_path, t_game *game)
 		error_event("Error\nValidating arguments", game, 1);
 	if (ft_strncmp(map_path + strlen(map_path) - 4, ".ber", 4) != 0)
 		error_event("Error\nMap file must be .ber", game, 1);
-	game->map.map_path = "maps/map4.ber";
+	game->map.map_path = map_path;
 	game->size_img = 64;
 	game->valid_char = "ECPT10";
 	game->player.img = NULL;
